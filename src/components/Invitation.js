090bb36049acdf98b3c3fb5656ad3e010e75aef5
Muhanned Abdulhaq Mohammed Noman <muhanned.noman@gmail.com>
2018-10-12 13:45:19 +0000

    Done.

--- src/components/Invitation.js
+++ src/components/Invitation.js
@@ -1,1 +1,17 @@
 // Code Invitation Component Here
+
+import React from 'react';
+// import React, { Component } from 'react' // We need to import react so we can make use of its .component class
+// import ReactDOM from 'react-dom' // ...and we need to import ReactDOM so we can create and test a virtual DOM with react!
+
+
+export default class Invitation  extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>You've been invited!</h1>
+        {this.props.children}
+      </div>
+    );
+  }
+}
